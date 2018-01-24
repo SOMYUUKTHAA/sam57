72c02e6101b6f30e3aa712f20cbd5119809386fc
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-24 16:19:54 +0530

    Create sam57.C

---
+++ sam57.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int a,int b);
+void main()
+{
+    int N,K,p;
+    printf("ENTER TWO VALUE\n");
+    scanf("\t%d",&N);
+    scanf("\t%d",&K);
+    printf("\nBEFORE SWAPPING THE VALUES ARE:\t%d\t%d",N,K);  
+    swap(N,K);
+}
+void swap(int a,int b)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    printf("\nVALUES ARE SWAPPED:\t%d\t%d",a,b);
+}
+    
